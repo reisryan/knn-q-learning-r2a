808a8569afc98109d8e80325e70e6f22b3d11cc1
Ryan <211036132@aluno.unb.br>
2024-09-04 11:58:38 -0300

    classes qlearn and knn

--- r2anewalgorithm1.py
+++ r2anewalgorithm1.py
@@ -9,65 +9,108 @@ from player.parser import *
 from sklearn.neighbors import KNeighborsRegressor as KnnR
 import numpy as np 
 
+class KNNModel:
+    def __init__(self, n_neighbors=5, weights='uniform'):
+        """
+        Inicializa o modelo KNN.
+        
+        :param n_neighbors: Número de vizinhos a serem considerados.
+        :param weights: Tipo de ponderação ('uniform' ou 'distance').
+        """
+        self.n_neighbors = n_neighbors
+        self.weights = weights
+        self.model = KNeighborsRegressor(n_neighbors=self.n_neighbors, weights=self.weights)
+
+    def fit(self, X, y):
+        """
+        Treina o modelo KNN nos dados de treino.
+        
+        :param X: Conjunto de características (features).
+        :param y: Conjunto de rótulos (targets).
+        """
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        """
+        Faz previsões nos dados fornecidos.
+        
+        :param X: Conjunto de características (features) para previsão.
+        :return: Previsões do modelo.
+        """
+        return self.model.predict(X)
+
+    def evaluate(self, X_test, y_test):
+        """
+        Avalia o modelo usando o erro quadrático médio (MSE).
+        
+        :param X_test: Conjunto de características de teste.
+        :param y_test: Conjunto de rótulos de teste.
+        :return: O MSE das previsões do modelo.
+        """
+        predictions = self.predict(X_test)
+        mse = mean_squared_error(y_test, predictions)
+        return mse
+
+class Qlearn:
+    def __init__(self, s, a):
+        self.s = np.array(s)
+        self.a = np.array(a)
+        self.Q = np.zeros((self.s).size,(self.a).size)
+        KNN = KNNModel()
+        self.alpha, self.beta, self.gamma, self.e = 0.5, 0,4, 0,15, 0.2 # gtaxa de aprendizado, fator de desconto e taxa de exploração
+
+    def choose_action(self, s):
+        np.argmax(self.Q[s])
+
+    def update(self, s, a, r, next):
+        if np.isin(s, self.s):
+            #upd = r + self.beta * max(self.Q[next,b] - self.Q[s, a])
+            #self.Q[s,a] = self.Q[s,a] + self.alpha * upd
+            pass
+        elif not np.isin(s, self.s):
+            #wk = 
+            #upd = r + self.beta * sum(w'k*Q(KNN(next),b)) - sum(wk*Q(KNNt,b))
+            #self.Q[KNN,a] = self.Q[KNN,a] + self.alpha*upd
+            pass 
+
+    def train(self, e, env):
+        """
+        Treina o agente em um determinado ambiente.
+        
+        :param e: Número de episódios de treinamento.
+        :param env: O ambiente de treinamento, que deve ter métodos `reset`, `step` e `render`.
+        """
+        for episode in range(episodes):
+            state = env.reset()
+            done = False
+            
+            while not done:
+                action = self.choose_action(state)
+                next_state, reward, done, info = env.step(action)
+                
+                self.update_q_table(state, action, reward, next_state)
+                state = next_state
+                
+                env.render()
 
 class R2ANewAlgorithm1(IR2A):
 
     def __init__(self, id):
-        IR2A.__init__(self, id) #herda o IR2A e seus métodos abstratos
-        # definir o (state s, action a, reward r)
-        # definir a tabela Q (com as tuplas state, action)
-        # deve existir uma atualização para a tabela Q a cada nova ação, com penalidade e taxa de aprendizado
-        self.qi, self.bd, self.br, self.b, self.td, self.fi, self.r = [], [], [], [], [], [], [] # lista de qualidades, bandwidth, ultimas qualidades, buffer, penalidades, recompensas
-        self.X = 0
-        t = 0
-        s_train = [] # Estados s(t) # s_train
-        self.Q = np.zeros((0,0))
-        # DEFINIR AINDA : TABELA Q DO Q-LEARNING, COM ZEROS (S(T), A(T))
-
-        q_train = self.Q # Estados Q(s(t), a(t)) # q_train
-
-        self.alpha, self.gamma, self.episilon = 0.1, 0.9, 0.1
-        # 2. Inicialize o KNN Regressor
-        knn = KnnR(n_neighbors=1)
-
-        # 3. Treine o modelo com os estados anteriores e valores Q
-        #knn.fit(s_train, q_train)
-
-
+        IR2A.__init__(self, id) #herda o IR2A e seus métodos abstratos        
+        
     def handle_xml_request(self, msg):
+        # definir o (state s, action a, reward r)
+        self.Q = Qlearn(s, a)
         self.send_down(msg) # envia até a Camada Inferior (ConnectionHandler)
 
     def handle_xml_response(self, msg):
-        # self.b.append() # calcular o buffer
-        self.bd.append(msg.get_quality_id())
-        self.parsed_mpd = parse_mpd(msg.get_payload()) # parser mpd info
-        self.qi = self.parsed_mpd.get_qi() # lista de qualidades possíveis
-        if self.qi and self.bd and self.br and self.b and self.td:
-            self.Q = np.array((len(self.b)*len(self.bd)*len(self.br)*len(self.td), len(self.qi)))
-            print(self.Q)
-        
+                
         self.send_up(msg) # envia até a Camada Superior (Player)
 
     def handle_segment_size_request(self, msg):
-        # recebe o próximo segmento de vídeo, sem informar a qualidade.
-        # x = qualidade definida / definição do algoritmo Q learning
-        # self.X = 
-        # adicionar aqui uma previsão para o s(t)
-        msg.add_quality_id(self.qi[self.X]) #define a qualidade conforme o knn q-learning
-        self.br.append(self.X) # ultima qualidade definida BR
         self.send_down(msg) # envia até a Camada Inferior (ConnectionHandler)
 
     def handle_segment_size_response(self, msg):
-        self.td.append( self.br[t] * 1 / self.bd[t] ) # time download = BR * TimeSegment(1) / Bandwidth
-        # self.b.append(max(0,(self.b[t] - Td)) + 1) # atualiza o valor do buffer
-        self.fi.append(min(self.alpha*max(0,(Td - self.b[t])), 1) + self.gamma*(max(self.b[t-2] - self.b[t-1], 0))**2) # atualizar a penalidade
-        self.r.append( self.br[t] - self.alpha*abs(self.br[t] - self.br[t-1]) - self.fi[t] ) # atualizar a recompensa
-        #E = sum((lambda_ ** t) * self.r[t] for t in range(len(self.r))) / len(self.r)
-
-        # a representação segmento ss é recebido pelo IR2A, deve-se executar as instruções para o correto funcionamento;
-        # tamanho do segmento recebido = msg.get_bit_length()
-        # diferenca de tempo transcorrido entre o momento da requisicao realizada ( handle_segment_size_request() )
-        # e a resposta recebida ( handle_segment_size_response() )
         self.send_up(msg) # envia até a Camada Superior (Player)
 
     def initialize(self):
